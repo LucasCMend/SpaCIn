800b70f3fd637d0ebe7a716c735734c91ebb5d17
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-08-06 20:30:11 -0300

    Adiciona Inimigos.py

---
+++ Inimigos.py
@@ -0,0 +1,61 @@
+import pygame
+
+
+class TiroInimigo(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+
+        super().__init__()
+
+        self.image = pygame.Surface((4, 15))
+        self.image.fill((255, 0, 0))
+
+        self.rect = self.image.get_rect()
+
+        self.rect.center = (x, y)
+        self.velocidade = 0.00008
+
+    def update(self):
+
+        self.rect.y += self.velocidade
+
+        if self.rect.top > 500: 
+            self.kill()
+
+
+
+class Inimigos(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+
+        super().__init__()
+
+        self.image = pygame.image.load("NaveInimiga.jpeg")
+        self.image = pygame.transform.scale(self.image, (50, 40))
+
+        self.rect = self.image.get_rect()
+
+        self.rect.topleft = (x, y)
+
+        self.velocidade_x = 0.009
+        self.velocidade_y = 0.007
+
+    def atirar(self):
+
+        return TiroInimigo(self.rect.centerx, self.rect.bottom)
+    
+    def update(self):
+
+        self.rect.x += self.velocidade_x
+        self.rect.y += self.velocidade_y
+
+        if self.rect.x <= 15 or self.rect.x >= 735:
+
+            self.velocidade_x *= -1
+
+        if self.rect.y >= 500:
+
+            self.kill()
+
+
+        
